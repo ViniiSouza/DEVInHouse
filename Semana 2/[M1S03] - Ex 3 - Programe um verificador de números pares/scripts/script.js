14cede1628b5076e6ef9024cd1b1d18255d389f1
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-11-19 11:10:14 -0300

    Ex 3 finalizado

--- Semana 2/[M1S03] - Ex 3 - Programe um verificador de números pares/scripts/script.js
+++ Semana 2/[M1S03] - Ex 3 - Programe um verificador de números pares/scripts/script.js
@@ -1,1 +1,13 @@
 //
+function verificarNumero(){
+    var numeroUsuario = parseInt(document.querySelector("#inputUsuario").value);
+
+    var total=numeroUsuario/2;
+    if(numeroUsuario % 2 == 0){
+        document.getElementById("resultado").textContent = "Par";
+    }
+    if(numeroUsuario % 2 == 1){
+        document.getElementById("resultado").textContent = "Ímpar";
+    }
+    
+}
