7426dcaa6c743dd5f75b3a059ff0cc799e7c319c
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-11-20 18:37:19 -0300

    Ex 8 finalizado

--- Semana 2/[M1S03] - Ex 8 - Salve a lista de mercado/scripts/script.js
+++ Semana 2/[M1S03] - Ex 8 - Salve a lista de mercado/scripts/script.js
@@ -1,1 +1,27 @@
 //
+
+function adicionarItem(){
+    var textoAdicionar = document.getElementById('textoAdicionar').value;
+    if(textoAdicionar.length < 1){
+        alert('O texto está vazio');
+    }
+    else{
+        criarItemLista(textoAdicionar);
+    }
+}
+
+function criarItemLista(texto){
+    var itemLista = document.createElement('li');
+    itemLista.textContent= texto;
+    var itemSelect = document.createElement('option');
+    itemSelect.textContent = texto;
+    
+    itemLista.classList.add('item-lista');
+    document.getElementById('lista-itens').appendChild(itemLista);
+    document.getElementById('lista-select').appendChild(itemSelect);
+
+}   
+
+function salvarItens(){
+
+}
