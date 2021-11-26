dc52a119502d3cb28534a856a498b75bb7442c9b
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-11-25 22:02:22 -0300

    Ex1 finalizado

---
+++ Semana 5/[M1S05] - Ex 1 - Crie uma classe Retangulo/scripts/script.js
@@ -0,0 +1,9 @@
+
+//
+class Retangulo{
+    constructor(altura, largura){
+        this.altura = altura; this.largura = largura;
+    }
+}
+
+var vetor = [new Retangulo(10, 10), new Retangulo(10,10), new Retangulo(10,10)]
