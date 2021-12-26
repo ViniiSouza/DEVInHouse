28c7a578f4ef2c5a65311fc974dfe4223066ccfa
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-12-26 11:21:39 -0300

    Ex5 finalizado

---
+++ Semana8/Ex5/portfolio-github/src/components/Repository/Repository.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+export const Repository = ({titulo, descricao, destacar}) => {
+    return (
+        <article>
+            <h3>{titulo}</h3>
+            <span>Projeto em destaque!</span>
+            <p>{descricao}</p>
+        </article>
+    )
+}
+
+Repository.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    descricao: PropTypes.string,
+    destacar: PropTypes.bool
+}
