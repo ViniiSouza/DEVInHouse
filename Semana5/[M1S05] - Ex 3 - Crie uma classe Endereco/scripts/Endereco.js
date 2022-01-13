df872d51544200ea922b00d9c6f420b1e67ab837
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-01-12 23:05:51 -0300

    Ex3 finalizado

--- Semana5/[M1S05] - Ex 3 - Crie uma classe Endereco/scripts/Endereco.js
+++ Semana5/[M1S05] - Ex 3 - Crie uma classe Endereco/scripts/Endereco.js
@@ -1,8 +1,15 @@
-// ver sobre gets and sets - o que é
 export default class Endereco {
     constructor(logradouro, numero, cidade, estado, pais, cep) {
         const params = [logradouro, numero, cidade, estado, pais, cep]
         if (params.some(p => typeof p !== 'string'))
-            throw 'Endereço inválido'
+            throw new Error('Endereço inválido! Revise os dados!')
+        else {
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.cidade = cidade;
+            this.estado = estado;
+            this.pais = pais;
+            this.cep = cep;
+        }
     }
 }
--- Semana5/[M1S05] - Ex 3 - Crie uma classe Endereco/scripts/script.js
+++ Semana5/[M1S05] - Ex 3 - Crie uma classe Endereco/scripts/script.js
@@ -1,23 +1,16 @@
 //
-import Endereco from "./Endereco";
+import Endereco from "./Endereco.js";
 
-
-function validarEndereco(){
-
-    document.getElementById('save-button').addEventListener('click', function(){
-        alert('teste')
-        var logradouro = pegarElemento('logradouro').value;
-        var numero = pegarElemento('numero').value;
-        var cidade = pegarElemento('cidade').value;
-        var estado = pegarElemento('estado').value;
-        var pais = pegarElemento('pais').value;
-        var cep = pegarElemento('cep').value;
-        var endereco = new Endereco(logradouro, numero, cidade, estado, pais, cep);
-        console.log(endereco)
-    })
-    
-
-}
+document.getElementById('save-button').addEventListener('click', function(){
+    var logradouro = pegarElemento('logradouro').value != '' ? pegarElemento('logradouro').value : null;
+    var numero = pegarElemento('numero').value != '' ? pegarElemento('numero').value : null;
+    var cidade = pegarElemento('cidade').value != '' ? pegarElemento('cidade').value : null;
+    var estado = pegarElemento('estado').value != '' ? pegarElemento('estado').value : null;
+    var pais = pegarElemento('pais').value != '' ? pegarElemento('pais').value : null;
+    var cep = pegarElemento('cep').value != '' ? pegarElemento('cep').value : null;
+    var endereco = new Endereco(logradouro, numero, cidade, estado, pais, cep);
+    console.log(endereco)
+})
 
 function pegarElemento(id){
     return document.getElementById(id);
