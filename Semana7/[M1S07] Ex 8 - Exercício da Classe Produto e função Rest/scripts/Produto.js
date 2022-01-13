a1dd82f2c2d60ec5f6fcc5867656c8dc48cf1edc
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-01-12 22:17:54 -0300

    Ex8 finalizado

---
+++ Semana7/[M1S07] Ex 8 - Exercício da Classe Produto e função Rest/scripts/Produto.js
@@ -0,0 +1,11 @@
+export class Produto{
+    nome;
+    quantidade;
+    valor
+    constructor (nome, quantidade, valor) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+}
