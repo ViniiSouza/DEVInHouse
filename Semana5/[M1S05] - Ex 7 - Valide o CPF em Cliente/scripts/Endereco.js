17769d7ee46a15e891ae445a52e2c2c223105dd6
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-01-15 20:28:37 -0300

    Ex7 finalizado

---
+++ Semana5/[M1S05] - Ex 7 - Valide o CPF em Cliente/scripts/Endereco.js
@@ -0,0 +1,19 @@
+export default class Endereco {
+    constructor(logradouro, numero, cidade, estado, pais, cep) {
+        const params = [logradouro, numero, cidade, estado, pais, cep]
+        if (params.some(p => typeof p !== 'string'))
+            throw new Error('Endereço inválido! Revise os dados!')
+        else {
+            let myRegex = /^\d{5}[-]\d{3}/
+            if (!myRegex.test(cep)) {
+                throw new Error('Formato de CEP inválido')
+            }
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.cidade = cidade;
+            this.estado = estado;
+            this.pais = pais;
+            this.cep = cep;
+        }
+    }
+}
