fbcef2d1a2e6cbd1a983ae59c3d25f8cc7e3327c
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-01-16 22:11:22 -0300

    Ex8 finalizado

--- Semana8/Ex8/portfolio-github/src/App.js
+++ Semana8/Ex8/portfolio-github/src/App.js
@@ -5,11 +5,11 @@ import { Summary } from './components/Summary/Summary';
 
 function App() {
   const lista_repositorios = [];
-  lista_repositorios.push({id: 1, titulo: 'Primeiro', descricao: 'descrição primeiro'});
-  lista_repositorios.push({id: 2, titulo: 'Segundo', descricao: 'descrição segundo'});
-  lista_repositorios.push({id: 3, titulo: 'Terceiro', descricao: 'descrição terceiro'});
-  lista_repositorios.push({id: 4, titulo: 'Quarto', descricao: 'descrição quarto'});
-  lista_repositorios.push({id: 5, titulo: 'Quinto', descricao: 'descrição quinto'});
+  lista_repositorios.push({id: 1, titulo: 'Primeiro', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc leo, placerat sed vulputate non, mollis dapibus enim. Phasellus in tincidunt mauris. Donec id ipsum blandit, lobortis est vitae, malesuada ex. Mauris sollicitudin mi felis, a elementum dui molestie sit amet. Curabitur vitae velit malesuada, rhoncus ipsum ac, blandit erat.'}); 
+  lista_repositorios.push({id: 2, titulo: 'Segundo', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc leo, placerat sed vulputate non, mollis dapibus enim. Phasellus in tincidunt mauris. Donec id ipsum blandit, lobortis est vitae, malesuada ex. Mauris sollicitudin mi felis, a elementum dui molestie sit amet. Curabitur vitae velit malesuada, rhoncus ipsum ac, blandit erat.'});
+  lista_repositorios.push({id: 3, titulo: 'Terceiro', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc leo, placerat sed vulputate non, mollis dapibus enim. Phasellus in tincidunt mauris. Donec id ipsum blandit, lobortis est vitae, malesuada ex. Mauris sollicitudin mi felis, a elementum dui molestie sit amet. Curabitur vitae velit malesuada, rhoncus ipsum ac, blandit erat.'});
+  lista_repositorios.push({id: 4, titulo: 'Quarto', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc leo, placerat sed vulputate non, mollis dapibus enim. Phasellus in tincidunt mauris. Donec id ipsum blandit, lobortis est vitae, malesuada ex. Mauris sollicitudin mi felis, a elementum dui molestie sit amet. Curabitur vitae velit malesuada, rhoncus ipsum ac, blandit erat.'});
+  lista_repositorios.push({id: 5, titulo: 'Quinto', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc leo, placerat sed vulputate non, mollis dapibus enim. Phasellus in tincidunt mauris. Donec id ipsum blandit, lobortis est vitae, malesuada ex. Mauris sollicitudin mi felis, a elementum dui molestie sit amet. Curabitur vitae velit malesuada, rhoncus ipsum ac, blandit erat.'});
 
   return (
     <div className="App">
@@ -21,7 +21,10 @@ function App() {
       (
         <div id="repo-list">
         {lista_repositorios.map((repo) =>
-          <p className="repo-item" key={repo.id}>{`> ${repo.id} - ${repo.titulo} - ${repo.descricao}`}</p>
+          <div className="repo-item" key={repo.id}>
+            <h5 className="repo-item-title">{`${repo.id} - ${repo.titulo}`}</h5>
+            <p className="repo-item-description">{repo.descricao}</p>
+          </div>
           )}
         </div>
       )
