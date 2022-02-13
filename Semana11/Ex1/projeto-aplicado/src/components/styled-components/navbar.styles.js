cb8dcc8d56faee6b4ebd219bf8e359a2dc28fe96
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-13 12:23:59 -0300

    Ex1 finalizado

---
+++ Semana11/Ex1/projeto-aplicado/src/components/styled-components/navbar.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const NavBar = styled.nav`
+    font-family: Calibri;    
+    border: 2px solid black;
+    border-radius: 10px;
+    background-color: ${(props) => props.theme.colors.secondary};
+    color: ${(props) => props.theme.colors.primary};
+`;
+
+export const Title = styled.h1`
+    font-size: 32px;
+`;
+
+export const ChangeTheme = styled.button`
+    font-family: Calibri;
+    font-size: 15px;
+    background-color: white;
+    font-weight: bold;
+    margin-bottom: 10px;
+    border-radius: 5px;
+`;
