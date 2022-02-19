98e0adb83c90ca8889981ab7ea06473cf09d04a7
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 14:05:19 -0300

    Ex5 finalizado

---
+++ Modulo2/Ex5/Ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> listaNumeros = new List<float>();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite um número:");
+                var numero = Console.ReadLine();
+                listaNumeros.Add(float.Parse(numero));
+            }
+            listaNumeros.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine($"Sequência decrescente: {string.Join(",", listaNumeros)}");
+        }
+    }
+}
