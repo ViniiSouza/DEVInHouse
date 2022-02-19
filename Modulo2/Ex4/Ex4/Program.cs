86532964e2ca65f950f45781682ccc3765af3843
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 11:56:30 -0300

    Ex4 finalizado

---
+++ Modulo2/Ex4/Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float menorNumero = float.MaxValue;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Digite um número: ");
+                var numero = Console.ReadLine();
+                if (float.Parse(numero) < menorNumero)
+                {
+                    menorNumero = float.Parse(numero);
+                }
+            }
+            Console.WriteLine($"O menor número digitado foi: {menorNumero}");
+        }
+    }
+}
