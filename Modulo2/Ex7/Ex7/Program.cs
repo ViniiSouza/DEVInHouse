97f41ac74f2a5281c3572ab78db3316c518853c4
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 15:05:51 -0300

    Ex7 finalizado

---
+++ Modulo2/Ex7/Ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            var numero = Console.ReadLine();
+            var numeroConvertido = int.Parse(numero);
+            var resultado = 0;
+            while (numeroConvertido > 0)
+            {
+                resultado = resultado * 10 + numeroConvertido % 10;
+                numeroConvertido /= 10;
+            }
+            Console.WriteLine($"O número invertido é: {resultado}");
+        }
+    }
+}
