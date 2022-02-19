a0c371628b460f616c4e1c45bde9cfe59b28630a
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 14:50:54 -0300

    Ex6 finalizado

---
+++ Modulo2/Ex6/Ex6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> lados = new List<float>();
+            
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Digite o {i}° lado do triângulo");
+                var lado = Console.ReadLine();
+                lados.Add(float.Parse(lado));
+            }
+            var lado1 = lados[0];
+            var lado2 = lados[1];
+            var lado3 = lados[2];
+            if (lado1 + lado2 < lado3 || lado1 + lado3 < lado2 || lado2 + lado3 < lado1)
+            {
+                Console.WriteLine("Triângulo inválido!");
+            }
+            else if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("O triângulo é equilátero!");
+            }
+            else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3)
+            {
+                Console.WriteLine("O triângulo é escaleno!");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("O triângulo é Isósceles!");
+            }
+        }
+    }
+}
