c155cbcf3940366e2beb9142a1cd4f45d02cb5cf
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-18 22:14:03 -0300

    Ex1 finalizado

---
+++ Modulo2/Ex1/Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a área do círculo");
+            var raio = Console.ReadLine();
+            var raioConvertido = Convert.ToInt32(raio);
+            var area = Math.PI * (raioConvertido * 2);
+            Console.WriteLine($"O círculo tem uma área de {area}²");
+        }
+    }
+}
