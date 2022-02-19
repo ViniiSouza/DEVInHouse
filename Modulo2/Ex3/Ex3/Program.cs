a18405036c9e25e5ac7131fbe8e87e9ca2472699
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 11:48:57 -0300

    Ex3 finalizado

---
+++ Modulo2/Ex3/Ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float maiorNumero = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite um número");
+                var numero = Console.ReadLine();
+                if (float.Parse(numero) > maiorNumero)
+                {
+                    maiorNumero = float.Parse(numero);
+                }
+            }
+            Console.WriteLine($"O maior número digitado foi: {maiorNumero}");
+        }
+    }
+}
