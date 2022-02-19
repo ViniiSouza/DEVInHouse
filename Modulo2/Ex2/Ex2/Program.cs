37c5aa73cf50a0fcb0c3a36716e1c0caa2edd547
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-02-19 11:01:41 -0300

    Ex2 finalizado

---
+++ Modulo2/Ex2/Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperatura em celsius:");
+            var temperatura = Console.ReadLine();
+            var temperaturaParseada = float.Parse(temperatura.Replace(".",","));
+            var temperaturaConvertida = temperaturaParseada * 33.8;
+            Console.WriteLine($"{temperaturaParseada}° celsius equivalem a {Math.Round(temperaturaConvertida, 2)}° fahrenheit");
+        }
+    }
+}
