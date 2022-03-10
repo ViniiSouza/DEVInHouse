580e8f774b5709a2087f33b6be9db27c31bf971b
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-03-10 10:12:14 -0300

    Ex2 finalizado

---
+++ Modulo2/Semana5/Ex2/ClassLibrary/Vehicle/RentedVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class RentedVehicle
+    {
+        public int Id { get; set; }
+
+        public User Client { get; set; }
+
+        public DateTime RentStart { get; set; }
+
+        public DateTime? RentEnd { get; set; }
+    }
+}
