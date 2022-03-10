cecc78fbe5bb959a3811d5ccf626acc0eca585d2
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-03-10 10:55:13 -0300

    Ex3 finalizado

---
+++ Modulo2/Semana5/Ex3/Ex3/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex3
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public int ExpectedLifeAge { get; set; }
+
+        public string Alimentation { get; set; }
+
+        public string EcoSystem { get; set; }
+
+        public string LocomotionType { get; set; }
+    }
+}
