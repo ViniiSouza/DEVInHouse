c27a2eebb21557483e1249f73319b72c2e66a533
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-03-11 10:15:51 -0300

    Ex4 finalizado

---
+++ Modulo2/Semana5/Ex4/Ex4/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    public class Account
+    {
+        public string AccountHash { get; set; }
+
+        public int SecurityCode { get; set; }
+
+        public int AccountNumber { get; set; }
+
+        public int AccountDigit { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public string PasswordEncrypted { get; set; }
+
+        public decimal TotalInvestments { get; set; }
+    }
+}
