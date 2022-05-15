2274745af5383c8d49c0ebaf2303ae5ee43c33a4
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-15 18:49:14 -0300

    Ex3 finalizado

---
+++ Modulo2/Semana12/Ex1/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ex1.Models
+{
+    public class Produto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string NomeProduto { get; set; }
+
+        [Required]
+        public float ValorProduto { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime? DataCompra { get; set; }
+    }
+}
