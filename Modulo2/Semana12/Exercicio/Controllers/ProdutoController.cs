bca43be2ef5e1c022561a348fe3d168d43e4e98e
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-21 19:43:38 -0300

    Ex4 finalizado

--- Modulo2/Semana12/Exercicio/Controllers/ProdutoController.cs
+++ Modulo2/Semana12/Exercicio/Controllers/ProdutoController.cs
@@ -20,10 +20,51 @@ namespace Ex1.Controllers
             return Ok(_context.Produto.ToList());
         }
 
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var produto = _context.Produto.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
         [HttpPost]
         public IActionResult Post([FromBody] Produto produto)
         {
             _context.Produto.Add(produto);
+            _context.SaveChanges();
+            return Ok(new
+            {
+                NomeProduto = produto.NomeProduto,
+                ValorProduto = produto.ValorProduto
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Produto produto, [FromRoute] int id)
+        {
+            produto.Id = id;
+            _context.Produto.Update(produto);
+            _context.SaveChanges();
+            return Ok(new
+            {
+                NomeProduto = produto.NomeProduto,
+                ValorProduto = produto.ValorProduto
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = _context.Produto.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produto.Remove(produto);
             _context.SaveChanges();
             return Ok(new
             {
