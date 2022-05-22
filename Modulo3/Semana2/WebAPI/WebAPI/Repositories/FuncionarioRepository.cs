05af927008b6eac785212588bb3267a9a39ea1a9
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-22 00:17:29 -0300

    Ex4 finalizado

---
+++ Modulo3/Semana2/WebAPI/WebAPI/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,39 @@
+﻿using RH.Models;
+
+namespace RH.Repository
+{
+    public static class FuncionarioRepository
+    {
+        public static List<Funcionario> Funcionarios = new() { new Funcionario() { Id = 1, Nome = "admin", Permissao = Enums.Permissoes.Administrador, Senha = "admin" } };
+
+        public static List<Funcionario> Obter()
+        {
+            return Funcionarios;
+        }
+
+        public static Funcionario? ObterPorUsuarioESenha(string nome, string senha)
+        {
+            return Funcionarios.Find(f => f.Nome == nome && f.Senha == senha);
+        }
+        
+        public static void Adicionar(Funcionario funcionario)
+        {
+            var nextId = Funcionarios.Last().Id++;
+            Funcionarios.Add(funcionario);
+        }
+
+        public static Funcionario? Editar(Funcionario funcionario, int id)
+        {
+            var indexFuncionario = Funcionarios.FindIndex(find => find.Id == id);
+            if (indexFuncionario == -1) return null;
+            funcionario.Id = id;
+            Funcionarios[indexFuncionario] = funcionario;
+            return funcionario;
+        }
+
+        public static void Excluir(Funcionario funcionario)
+        {
+            Funcionarios.Remove(funcionario);
+        }
+    }
+}
