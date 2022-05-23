4de5f3103d8705d57277f34a8f88aaec90cc2f8d
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-22 22:10:28 -0300

    Ex7 finalizado

---
+++ Modulo3/Semana2/WebAPI/WebAPI/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RH.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RH
+{
+    public class TokenService
+    {
+        public string GerarToken(Funcionario funcionario)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, funcionario.Id.ToString()),
+                    new Claim(ClaimTypes.Role, funcionario.Permissao.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
